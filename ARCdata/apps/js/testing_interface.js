f479223a7d27ad72344fea91c7d63304b6d86fc9
ken <kchiem12@gmail.com>
2022-07-06 11:18:21 -0400

    add larger textbox for inputting bitmaps

--- ARCdata/apps/js/testing_interface.js
+++ ARCdata/apps/js/testing_interface.js
@@ -769,4 +769,20 @@ $(document).ready(function () {
       }
     }
   });
+
+  $('textarea').on('keydown', function(e) {
+    if (e.key == 'Tab') {
+      e.preventDefault();
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+  
+      // set textarea value to: text before caret + tab + text after caret
+      this.value = this.value.substring(0, start) +
+        "\t" + this.value.substring(end);
+  
+      // put caret at right position again
+      this.selectionStart =
+        this.selectionEnd = start + 1;
+    }
+  });
 });
